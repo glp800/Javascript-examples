f33499362cb207ea5ff12de1b4c9973d7349e530
centosl <2913626209@qq.com>
2019-10-20 15:35:17 +0800

    201910201535

---
+++ wechat/js/msg.js
@@ -0,0 +1,37 @@
+var list = [{
+	name: "幸福",
+	head_img: "https://dcloud.io/hellomui/images/shuijiao.jpg",
+	user_id: '2',
+	id: "125",
+	last_msg: '能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？'
+}, {
+	name: "木屋",
+	head_img: "https://dcloud.io/hellomui/images/muwu.jpg",
+	user_id: '2',
+	id: "125",
+	last_msg: '静静的看这个世界，最后终于疯了~'
+}, {
+	name: "昨天",
+	head_img: "https://dcloud.io/hellomui/images/yuantiao.jpg",
+	user_id: '2',
+	id: "125",
+	last_msg: '烤炉模式的城，到黄昏，如同打翻的调色盘一般.'
+}, {
+	name: "冷萌君",
+	head_img: "https://dcloud.io/hellomui/images/shuijiao.jpg",
+	user_id: '2',
+	id: "125",
+	last_msg: '约会吗？小哥哥'
+}, {
+	name: "刘备备",
+	head_img: "https://dcloud.io/hellomui/images/muwu.jpg",
+	user_id: '2',
+	id: "125",
+	last_msg: '能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？'
+}, {
+	name: "日向雏田",
+	head_img: "https://dcloud.io/hellomui/images/shuijiao.jpg",
+	user_id: '2',
+	id: "125",
+	last_msg: '迷失的人迷失了，相逢的人还会在相逢！'
+}]
