4e04f4f4fb1fce0e53a9cc9de9a9c6bbc0452483
centosl <send.lzp@qq.com>
2020-03-26 00:23:47 +0800

    202003260023

---
+++ vueDemo/demo2/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <form action="" @submit.prevent="login">
+      <input type="text" v-model="mobile">
+      <input type="password" v-model="password">
+      <button type="submit" :disabled="!isVaild">login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobile: "",
+      password: ""
+    };
+  },
+  computed: {
+    isVaild(){
+      return String(this.mobile).match(/^1\d{10}$/)&&String(this.password).length>=6
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.mobile);
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #f2f2f2;
+}
+input {
+  margin: 10px auto;
+  display: block;
+  width: 280px;
+  height: 32px;
+}
+button {
+  margin: 0 auto;
+  display: block;
+  width: 120px;
+  height: 30px;
+}
+input:focus {
+  outline: none;
+}
+</style>
